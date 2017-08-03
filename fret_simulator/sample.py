fa85384bc8c9ff756a373e0843df37717b6e9fda
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2017-08-03 10:54:12 +0200

    Rename sim_numpy -> time_trace

--- fret_simulator/sample.py
+++ fret_simulator/sample.py
@@ -3,7 +3,7 @@ import multiprocessing
 import numpy as np
 import scipy
 
-from . import sim_numpy
+from . import time_trace
 
 
 def sample_p_vals(life_times, efficiencies, exposure_time, data_points,
@@ -44,10 +44,10 @@ def sample_p_vals_numpy(life_times, efficiencies, exposure_time, data_points,
     ret = np.empty(life_times.shape[0], dtype=float)
     dur = data_points * exposure_time
     for i, (lt, ef) in enumerate(zip(life_times, efficiencies)):
-        truth = sim_numpy.two_state_truth(lt, ef, dur)
-        st, se = sim_numpy.sample(*truth, exposure_time, data_points)
-        d, a = sim_numpy.experiment(se, photons, donor_brightness,
-                                    acceptor_brightness)
+        truth = time_trace.two_state_truth(lt, ef, dur)
+        st, se = time_trace.sample(*truth, exposure_time, data_points)
+        d, a = time_trace.experiment(se, photons, donor_brightness,
+                                     acceptor_brightness)
         e = a / (d+a)
         ks, p = scipy.stats.ks_2samp(e, experiment_data)
         ret[i] = ks
