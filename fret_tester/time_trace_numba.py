63a753b2feae8767511af324b707be8fde1ae9b1
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2017-09-10 01:09:39 +0200

    time_trace_numba: Add missing functionality
    - `experiment` function
    - `DataSet` class
    - `simulate_dataset` function

--- fret_tester/time_trace_numba.py
+++ fret_tester/time_trace_numba.py
@@ -160,3 +160,108 @@ def sample(time, eff, exposure_time, data_points=np.inf):
 
         ret_e[dp] = intens / exposure_time
     return ret_t, ret_e
+
+
+@numba.njit
+def experiment(eff, photons, donor_brightness, acceptor_brightness):
+    """From sampled smFRET time traces, simulate (noisy) experiment results
+
+    This adds noise coming from the rather broad brightness distributions of
+    fluorophores to the time traces, resulting in a trace as one would get
+    out of an experiment.
+
+    Parameters
+    ----------
+    eff : array_like
+        Sampled FRET efficiencies
+    photons : float
+        Mean summed number of photons emitted by donor and acceptor per
+        exposure.
+    donor_brightness, acceptor_brightness : callable
+        Takes one argument, an array of mean brightness values. For each entry
+        `m` it returns a random value drawn from the brightness distribution
+        with mean `m`.
+    """
+    acc_p = eff * photons
+    don_p = (1 - eff) * photons
+    acc_p_noisy = acceptor_brightness.generate(acc_p)
+    don_p_noisy = donor_brightness.generate(don_p)
+    return don_p_noisy, acc_p_noisy
+
+
+@numba.jitclass([("true_time", numba.float64[:]),
+                 ("true_eff", numba.float64[:]),
+                 ("samp_time", numba.float64[:]),
+                 ("samp_eff", numba.float64[:]),
+                 ("exp_don", numba.float64[:]),
+                 ("exp_acc", numba.float64[:]),
+                 ("exp_eff", numba.float64[:])])
+class DataSet:
+    """Full data set of a simulation run
+
+    Attributes
+    ----------
+    true_time, true_eff : numpy.ndarray, dtype(float64)
+        True state trajectory as produced by :py:func:`two_state_truth`
+    samp_time, samp_eff : numpy.ndarray, dtype(float64)
+        Sampled state trajectory as produced by :py:func:`sample`
+    exp_don, exp_acc : numpy.ndarray, dtype(float64)
+        (Noisy) donor and acceptor intensities as produced by
+        :py:func:`experiment`
+    exp_eff : numpy.ndarray, dtype(float64)
+        Experiment FRET efficiency, i.e. ``exp_acc / (exp_acc + exp_don)``
+"""
+    def __init__(self, true_time, true_eff, samp_time, samp_eff, exp_don,
+                 exp_acc, exp_eff):
+        self.true_time = true_time
+        self.true_eff = true_eff
+        self.samp_time = samp_time
+        self.samp_eff = samp_eff
+        self.exp_don = exp_don
+        self.exp_acc = exp_acc
+        self.exp_eff = exp_eff
+
+
+@numba.njit
+def simulate_dataset(truth, exposure_time, data_points, photons,
+                     donor_brightness, acceptor_brightness):
+    """Simulate a whole data set
+
+    Consecutively run :py:func:`two_state_truth`, :py:func:`sample`, and
+    :py:func:`experiment`.
+
+    Parameters
+    ----------
+    truth : callable or tuple of numpy.ndarray, shape=(n,)
+        Ground truth smFRET time trace. If this is an array, ``truth[0]``
+        should be the sequence of time points where state transitions
+        occur and ``truth[1]`` the FRET efficiency between the (i-1)-th
+        time point (or 0 for i = 0) and the i-th time point.
+        If this is callable, it should return a ground truth smFRET time
+        trace as described above. It should take one float parameter, which is
+        the minimum duration of the generated trace.
+    exposure_time : float
+        Exposure time for sampling. All transition during a exposure will be
+        integrated.
+    data_points : scalar, optional
+        Number of data points to return. If the FRET time trace is too short,
+        the maximum number of data points the trace allows is returned.
+        Defaults to infinity.
+    photons : float
+        Mean summed number of photons emitted by donor and acceptor per
+        exposure.
+    donor_brightness, acceptor_brightness : callable
+        Takes one argument, an array of mean brightness values. For each entry
+        `m` it returns a random value drawn from the brightness distribution.
+
+    Returns
+    -------
+    DataSet
+        Collected simulated data
+    """
+    dur = data_points * exposure_time
+    t, e = truth.generate(dur)
+
+    st, se = sample(t, e, exposure_time, data_points)
+    d, a = experiment(se, photons, donor_brightness, acceptor_brightness)
+    return DataSet(t, e, st, se, d, a, a/(d+a))
