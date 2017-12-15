f367e62aaddb0abb0bda22275e71311cbe6c7472
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2017-12-15 16:09:21 +0100

    __init__: Import stuff from ks_test

--- fret_tester/__init__.py
+++ fret_tester/__init__.py
@@ -2,3 +2,5 @@ from .plot import Plotter1D, Plotter2D
 from .fluo_models import LognormalBrightness, PolyLnBrightness
 from .time_trace import (TwoStateExpTruth, sample, experiment,
                          DataSet, simulate_dataset)
+from .ks_test import (BatchTester, numpy_tester, batch_test,
+                      combine_test_results)
